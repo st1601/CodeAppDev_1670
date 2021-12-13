6f90e95ae541d42948a0a60d480cccc99d750b76
hiepdqgch18021 <68195041+hiepdqgch18021@users.noreply.github.com>
2021-12-13 08:00:20 +0700

    ok

--- Assignment/app.js
+++ Assignment/app.js
@@ -10,35 +10,33 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-    res.render('GradeTrainee')
+    res.render('index')
 })
 
 
-app.post('/add', async(req, res) => {
-    const idInput = req.body.txtID;
-    const nameInput = req.body.txtName;
-    const gmailInput = req.body.txtGmail;
-    const ageInput = req.body.txtAge;
-    const BirthdayInput = req.body.txtBirthday;
-    const newTrainee = { ID: idInput, Name: nameInput, Gmail: gmailInput, Age: ageInput, Birthday: BirthdayInput }
-    const client = await MongoClient.connect(url);
-    const dbo = client.db(DATABASE_NAME);
-    await dbo.collection("trainee").insertOne(newTrainee);
-    res.redirect("/");
-})
+// app.post('/add', async(req, res) => {
+//     const idInput = req.body.txtID;
+//     const nameInput = req.body.txtName;
+//     const gmailInput = req.body.txtGmail;
+//     const ageInput = req.body.txtAge;
+//     const BirthdayInput = req.body.txtBirthday;
+//     const newTrainee = { ID: idInput, Name: nameInput, Gmail: gmailInput, Age: ageInput, Birthday: BirthdayInput }
+//     const client = await MongoClient.connect(url);
+//     const dbo = client.db(DATABASE_NAME);
+//     await dbo.collection("trainee").insertOne(newTrainee);
+//     res.redirect("/");
+// })
 
-app.get('/', async(req, res) => {
-    const client = await MongoClient.connect(url);
-    const dbo = client.db(DATABASE_NAME)
-    const allTrainee = await dbo.collection("Trainee").find({}).toArray();
-    res.render('ListTrainee', { data: allTrainee });
-    return dbo;
-})
+// app.get('/', async(req, res) => {
+//     const client = await MongoClient.connect(url);
+//     const dbo = client.db(DATABASE_NAME)
+//     const allTrainee = await dbo.collection("Trainee").find({}).toArray();
+//     res.render('ListTrainee', { data: allTrainee });
+//     return dbo;
+// })
+const TrainerCtl = require('./controller/Trainer')
+app.use('/Trainer', TrainerCtl)
 
 const PORT = process.env.PORT || 5010
 app.listen(PORT)
 console.log("Server is running! " + PORT)
-
-
-// const TrainerCtl = require('./controller/TrainerController')
-// app.use('/TrainerController', TrainerCtl)
