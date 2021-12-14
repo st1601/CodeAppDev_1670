949e76b20af2b298f039411d06302efffb61fb69
hiepdqgch18021 <68195041+hiepdqgch18021@users.noreply.github.com>
2021-12-14 18:24:36 +0700

    add and delete grade

--- Assignment/Controller/Trainer.js
+++ Assignment/Controller/Trainer.js
@@ -1,12 +1,11 @@
-//const { insertObject } = require('../databaseHandel')
 const express = require('express')
 const router = express.Router()
 const DATABASE_NAME = "Appdev1670";
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://localhost:27017';
 
-router.get('/', (req, res) => {
-    res.render('ListTrainee')
+router.get('trainer', (req, res) => {
+    res.render('GradeTrainee')
 })
 
 // -- -- -- -- -- -- -- -- -- -- -- -- -- -- --GradeTrainee
@@ -16,11 +15,11 @@ router.get('/addGrade', (req, res) => {
 router.post('/addGrade', async(req, res) => {
     const nameInput = req.body.txtTraineeGrade;
     const typeGrade = req.body.typeGrade;
-    const newGrade = { Name: nameInput, Type: typeGrade }
+    const newGrade = { name: nameInput, TypeGrade: typeGrade }
     const client = await MongoClient.connect(url);
     const dbo = client.db(DATABASE_NAME);
     await dbo.collection("listGrade").insertOne(newGrade);
-    res.render("/");
+    res.redirect("/Trainer");
 })
 
 router.post('/searchGrade', async(req, res) => {
@@ -28,22 +27,23 @@ router.post('/searchGrade', async(req, res) => {
     const client = await MongoClient.connect(url);
     const dbo = client.db(DATABASE_NAME);
     const allGrade = await dbo.collection("listGrade").find({ Name: searchInput }).toArray();
-    res.render('GradeTrainee', { data: allGrade })
+    res.redirect("/Trainer")
 });
 
 router.get('/deleteGrade', async(req, res) => {
     const id = req.query.id;
+    var ObjectId = require('mongodb').ObjectId;
     const client = await MongoClient.connect(url);
     const dbo = client.db(DATABASE_NAME);
     await dbo.collection("listGrade").deleteOne({ "_id": ObjectId(id) });
-    res.redirect("/");
+    res.redirect("/Trainer");
 });
 
 router.get('/', async(req, res) => {
         const client = await MongoClient.connect(url);
-        const dbo = client.db(DATABASE_NAME)
-        const Grade = await dbo.collection("listGrade").find({}).toArray();
-        res.render('GradeTrainee', { data: Grade });
+        const dbo = client.db(DATABASE_NAME);
+        const allGrade = await dbo.collection("listGrade").find({}).toArray();
+        res.render('GradeTrainee', { data: allGrade });
         return dbo;
     })
     // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
